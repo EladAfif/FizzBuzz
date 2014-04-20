a0d617ebba3d7dfbeed41e4ed9aedfe6d98c6531
Elad Afif <elad.afif@gmail.com>
2014-04-20 11:53:11 +0300

    check without Init

--- github.com/EladAfif/FizzBuzz.git/FizBazz/src/fizzbazzTest.java
+++ github.com/EladAfif/FizzBuzz.git/FizBazz/src/fizzbazzTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 
 public class fizzbazzTest {
 
+	fizzbazz fizz = new fizzbazz();
 	@Test
-	public void test() {
-		
+	public void testInit() {
+		//fizz.initArray();
+		int i;
+		for (i = 0 ; i < 100 ; i++)
+		{
+			//System.out.println(fizz.a[i]);
+			if(fizz.a[i].equals(i))
+				fail();
+			
+		}
 		
 		
 		
