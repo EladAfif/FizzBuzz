527f68e277d3af18a788127f01771d2971330a91
Elad Afif <elad.afif@gmail.com>
2014-04-20 12:47:21 +0300

    total app FizzBazz

--- github.com/EladAfif/FizzBuzz.git/FizBazz/src/fizzbazzTest.java
+++ github.com/EladAfif/FizzBuzz.git/FizBazz/src/fizzbazzTest.java
@@ -30,8 +30,8 @@ public class fizzbazzTest {
 			//System.out.println(fizz.a[i]);
 				if(fizz.a[i] != "Fizz")
 					fail();
-				else 
-					System.out.println(fizz.a[i]);
+			//	else 
+			//		System.out.println(fizz.a[i]);
 		}
 	}
 	
@@ -46,8 +46,8 @@ public class fizzbazzTest {
 			//System.out.println(fizz.a[i]);
 				if(fizz.a[i] != "Buzz")
 					fail();
-				else 
-					System.out.println(fizz.a[i]);
+			//	else 
+			//		System.out.println(fizz.a[i]);
 		}
 	}
 	
@@ -62,8 +62,48 @@ public class fizzbazzTest {
 			//System.out.println(fizz.a[i]);
 				if(fizz.a[i] != "FizzBuzz")
 					fail();
-				else 
-					System.out.println(fizz.a[i]);
+			//	else 
+				//	System.out.println(fizz.a[i]);
+		}
+	}
+	
+	@Test
+	public void testIsTotalFizzBuzz() 
+	{
+		fizz.initArray();
+		fizz.isBuzz();
+		fizz.isFizz();
+		fizz.isFizzBuzz();
+		int i;
+		
+		for (i = 0 ; i < 100 ; i++)
+		{
+			System.out.println(fizz.a[i]);
+			if(i%5 == 0 && i%3 == 0)
+			{
+				//System.out.println(fizz.a[i]);
+					if(fizz.a[i] != "FizzBuzz")
+						fail();
+			}
+			else if(i%5 == 0)
+			{
+				//System.out.println(fizz.a[i]);
+					if(fizz.a[i] != "Buzz")
+						fail();
+			}
+			
+			else if(i%3 == 0)
+			{
+				//System.out.println(fizz.a[i]);
+					if(fizz.a[i] != "Fizz")
+						fail();
+			}
+			
+			else if(fizz.a[i].equals(i))
+			{
+					fail();
+			}
+			
 		}
 	}
 	
